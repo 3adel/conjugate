51f1b14395d738e7f08d6e24ef10ab4af27170f2
Adel Shehadeh <adel.sh@gmail.com>
2016-06-13 00:30:07 +0200

    trying to store conjugations and their categories in a dictionary WIPP

--- Conjugate/FirstViewController.h
+++ Conjugate/FirstViewController.h
@@ -8,13 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FirstViewController : UIViewController <UITextFieldDelegate>
+@interface FirstViewController : UIViewController <UITextFieldDelegate,UITableViewDelegate, UITableViewDataSource>
+
+
+
+//outlets
+@property (weak, nonatomic) IBOutlet UITableView *conjugationsTableView;
 
 
 @property (weak, nonatomic) IBOutlet UITextField *verbUITextField;
 @property (weak, nonatomic) IBOutlet UITextView *jsonResultsTextView;
 @property (strong, nonatomic) UITapGestureRecognizer *tapToDismissKeyboard;
 @property (strong, nonatomic) UIPanGestureRecognizer *panToDismissKeyboard;
+@property (weak, nonatomic) IBOutlet UITextView *conjugationResultsUITableView;
+
+
+
+@property (strong, nonatomic) NSMutableDictionary *sectionedConjugations;
+@property (weak, nonatomic) NSArray *conjugationSectionTitlesArray;
 
 
 - (void) conjugteWithString:(NSString *)string;
--- Conjugate/FirstViewController.m
+++ Conjugate/FirstViewController.m
@@ -10,17 +10,18 @@
 
 @interface FirstViewController (){
     NSString *verbAPISearchResult;
-}
+    
+        NSArray *conjugationSectionTitles;
+    }
 
 @end
 
 #define CONJUGATOR_BASE_URL 20
 
 
-
 @implementation FirstViewController
 
-
+@synthesize conjugationResultsUITableView = _conjugationResultsUITableView;
 #ifdef DEBUG
 NSString *const conjugatorBaseEndPoint = @"http://api.verbix.com/conjugator/json/eba16c29-e22e-11e5-be88-00089be4dcbc/deu/";
 NSString *const searchBaseEndPoint = @"http://api.verbix.com/finder/json/eba16c29-e22e-11e5-be88-00089be4dcbc/deu/";
@@ -31,21 +32,23 @@ NSString *const searchBaseEndPoint = @"http://api.verbix.com/finder/json/eba16c2
 
 @synthesize tapToDismissKeyboard = _tapToDismissKeyboard;
 @synthesize panToDismissKeyboard = _panToDismissKeyboard;
+@synthesize conjugationSectionTitlesArray =_conjugationSectionTitlesArray;
+@synthesize conjugationsTableView = _conjugationsTableView;
+@synthesize sectionedConjugations =_sectionedConjugations;
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.verbUITextField.delegate = self;
+    self.conjugationsTableView.delegate = self;
     
-
-
-        
-        _tapToDismissKeyboard = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
-        _panToDismissKeyboard =[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
-        
-
+    
+    _tapToDismissKeyboard = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+    _panToDismissKeyboard =[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+    
     [self.view addGestureRecognizer:_tapToDismissKeyboard];
-
-
+    
+    
 }
 
 
@@ -60,9 +63,7 @@ replacementString:(NSString *)string{
     NSLog(@"Word typed 1 = %@", newString);
     
     //start fitching stuff
-    //[self conjugteWithString:newString];
     [self searchVerbFormWithString:newString];
-    
     return YES;
 }
 
@@ -70,16 +71,14 @@ replacementString:(NSString *)string{
 - (NSString *) searchVerbFormWithString:(NSString *)string{
     
     verbAPISearchResult = [[NSString alloc] init];
-    
+
     //convert string to lower case. an API limitation
     NSString *lowerCaseString =[string lowercaseString];
     
     //stich the typed word to the base URL and encode in case there's some weird characters.
     NSURL *url = [[NSURL alloc] initWithString:[ [searchBaseEndPoint stringByAppendingString:lowerCaseString]stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLFragmentAllowedCharacterSet]]];
-
     
     NSLog(@"URL =%@ ",[searchBaseEndPoint stringByAppendingString:lowerCaseString]);
-    
     NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
     
@@ -103,48 +102,30 @@ replacementString:(NSString *)string{
                 
                 
                 //Data successfully received
-                
-                
                 dispatch_async(dispatch_get_main_queue(), ^{
-                    
-
-                    
-                    
                     
                     //A proper verb is found
                     if (returnedDict.count >0) {
-                         NSLog(@"Pure verb = %@", [returnedDict objectAtIndex:0]);
+                        NSLog(@"Pure verb = %@", [returnedDict objectAtIndex:0]);
                         verbAPISearchResult =[[returnedDict objectAtIndex:0]description];
-                        //self.jsonResultsTextView.text =[[returnedDict objectAtIndex:0]description];
                         
                         //now pull the dictionary and find the root verb. .
                         self.jsonResultsTextView.text =[[returnedDict objectAtIndex:0] valueForKey:@"verb"];
                         
                         //not take the search result and call the conjugator
-                        //e[self conjugteWithString:newString];
-                        
                         [self conjugteWithString:[[returnedDict objectAtIndex:0] valueForKey:@"verb"]];
                         
                         
                     }
-                   
-
                     
                 });
                 
-                
-                
             }
-            
-            
         }
     }];
-    
     // Resume the task.
     [task resume];
-    
     return nil;
-    
 }
 
 
@@ -155,11 +136,6 @@ replacementString:(NSString *)string{
     
     //stich the typed word to the base URL and encode in case there's some weird characters.
     NSURL *url = [[NSURL alloc] initWithString:[ [conjugatorBaseEndPoint stringByAppendingString:string]stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLFragmentAllowedCharacterSet]]];
-    
-    
-    //NSString *newCountryString =[@"fdd" stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
-
-    
     
     NSLog(@"URL =%@ ",[conjugatorBaseEndPoint stringByAppendingString:string]);
     
@@ -186,125 +162,139 @@ replacementString:(NSString *)string{
                 
                 
                 //Data successfully received
-               // NSLog(@"Data %@", returnedDict);
-                
                 dispatch_async(dispatch_get_main_queue(), ^{
                     
-                   //self.jsonResultsTextView.text =returnedDict.description;
                     
+                    //set sectioned conjugations
+                    /*
+                    _sectionedConjugations = @{@"B" : @[@"Bear", @"Black Swan", @"Buffalo"],
+                                @"C" : @[@"Camel", @"Cockatoo"],
+                                @"D" : @[@"Dog", @"Donkey"],
+                                @"E" : @[@"Emu"],
+                                @"G" : @[@"Giraffe", @"Greater Rhea"],
+                                @"H" : @[@"Hippopotamus", @"Horse"],
+                                @"K" : @[@"Koala"],
+                                @"L" : @[@"Lion", @"Llama"],
+                                @"M" : @[@"Manatus", @"Meerkat"],
+                                @"P" : @[@"Pandaaa", @"Peacock", @"Pig", @"Platypus", @"Polar Bear"],
+                                @"R" : @[@"Rhinoceros"],
+                                @"S" : @[@"Seagull"],
+                                @"T" : @[@"Tasmania Devil"],
+                                @"W" : @[@"Whale", @"Whale Shark", @"Wombat"]};
                     
-                    
+                    conjugationSectionTitles = [[_sectionedConjugations allKeys] sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+                    [_conjugationsTableView reloadData];
+                    */
+                    _sectionedConjugations = [[NSMutableDictionary alloc] init];
+
                     for ( int i =0;i<=22;i++ ) {
-                        
+  
                         //protect from nul
                         if ([[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",i]]valueForKey:@"forms"]) {
                             
-                            NSLog(@"%@", [[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",i]]valueForKey:@"name"]);
                             
+                            
+                            //set each section title
+             
+                            NSLog(@"%@ Type = %@",[NSString stringWithFormat:@"%d",i],[NSString stringWithFormat:@"%@",[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",i]]valueForKey:@"name"]] );
+                            [_sectionedConjugations setObject:@"Object..." forKey:[NSString stringWithFormat:@"%@",[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",i]]valueForKey:@"name"]]];
+                            
+                            
+                            [_conjugationsTableView reloadData];
+
+                            
+                            NSLog(@"%@", [[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",i]]valueForKey:@"name"]);
                             self.jsonResultsTextView.text = [self.jsonResultsTextView.text stringByAppendingString:[NSString stringWithFormat:@"\n\n%@",[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",i]]valueForKey:@"name"]]];
                             self.jsonResultsTextView.text = [self.jsonResultsTextView.text stringByAppendingString:@"\n------------------------"];
                             
                             //count how many elements in "forms" for each row
-                           // NSLog(@"How many elements in each category: %ld", [[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",i]]valueForKey:@"forms"]count]);
-                            
-                            
-                            
-                            
                             for (int j=0; j<[[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",i]]valueForKey:@"forms"]count]; j++) {
-                                
-                                
-                                
-                                
-                                
-                                
-                                
                                 if ([[[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",j]]valueForKey:@"forms"]objectAtIndex:j] valueForKey:@"form"]) {
                                     
                                     //make sure that form and pronoun are both not null
-
                                     if ([[[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",j]]valueForKey:@"forms"]objectAtIndex:j] valueForKey:@"pronoun"]) {
+                                        
                                         NSLog(@"%@: %@",[[[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",j]]valueForKey:@"forms"]objectAtIndex:j] valueForKey:@"pronoun"], [[[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",j]]valueForKey:@"forms"]objectAtIndex:j] valueForKey:@"form"]);
                                         
                                         
-                                            
-                                            self.jsonResultsTextView.text = [self.jsonResultsTextView.text stringByAppendingString:[NSString stringWithFormat:@"\n%@: %@",[[[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",j]]valueForKey:@"forms"]objectAtIndex:j] valueForKey:@"pronoun"], [[[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",j]]valueForKey:@"forms"]objectAtIndex:j] valueForKey:@"form"]]];
-                                            
+                                        self.jsonResultsTextView.text = [self.jsonResultsTextView.text stringByAppendingString:[NSString stringWithFormat:@"\n%@: %@",[[[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",j]]valueForKey:@"forms"]objectAtIndex:j] valueForKey:@"pronoun"], [[[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",j]]valueForKey:@"forms"]objectAtIndex:j] valueForKey:@"form"]]];
+                                        
 
-                                            
-                                            
-                                            
 
-                                        
-                                        
-                                        
                                     }
                                     
                                     else{
                                         NSLog(@"Form: %@", [[[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",j]]valueForKey:@"forms"]objectAtIndex:j] valueForKey:@"form"]);
-                                        
-                                        
-                                        
-                                        
+
                                         self.jsonResultsTextView.text = [self.jsonResultsTextView.text stringByAppendingString:[NSString stringWithFormat:@"\n%@", [[[[[returnedDict valueForKey:@"tenses"]valueForKey:[NSString stringWithFormat:@"%d",j]]valueForKey:@"forms"]objectAtIndex:j] valueForKey:@"form"]]];
                                         
                                     }
-                                    
-                                    
-                                    
-                                    
-                                    
-                                    
                                 }
-                                
-                                
-                                
-                                
-                                
-                                
-                                
+  
                             }
                             
-                            
-                            
-                            
-                            
-                            
-
                         }
-
-                        
-                        
-                        
-                        
+  
                     }
 
-
-
                 });
-                
-                
-                
+
             }
-            
-            
+ 
         }
     }];
     
     // Resume the task.
     [task resume];
-    
-    
 }
 
 
 
 -(void)dismissKeyboard {
-    
-
-        [self.verbUITextField resignFirstResponder];
-
-
-
+    [self.verbUITextField resignFirstResponder];
 }
+
+
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return [conjugationSectionTitles count];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return [conjugationSectionTitles objectAtIndex:section];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    NSString *sectionTitle = [conjugationSectionTitles objectAtIndex:section];
+    NSArray *sectionConjugations = [_sectionedConjugations objectForKey:sectionTitle];
+    return [sectionConjugations count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    if (cell ==nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    }
+    
+    // Configure the cell...
+    NSString *sectionTitle = [conjugationSectionTitles objectAtIndex:indexPath.section];
+    NSArray *sectionConjugations = [_sectionedConjugations objectForKey:sectionTitle];
+    NSString *animal = [sectionConjugations objectAtIndex:indexPath.row];
+    cell.textLabel.text = animal;
+    
+    return cell;
+}
+
+
 
 @end
