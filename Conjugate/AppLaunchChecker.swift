ea9bd2c06b92bc90e21b7cb1af776bdc137af024
Halil Gursoy <halil.gursoy@zalando-lounge.de>
2016-11-01 00:04:59 +0100

    Added a welcome verb on first install

---
+++ Conjugate/AppLaunchChecker.swift
@@ -0,0 +1,20 @@
+//
+//  AppLaunchChecker.swift
+//  Conjugate
+//
+//  Created by Halil Gursoy on 31/10/2016.
+//  Copyright © 2016 Adel  Shehadeh. All rights reserved.
+//
+
+import Foundation
+
+
+class AppLaunchChecker {
+    var isFirstInstall: Bool {
+        return UserDefaults.standard.value(forKey: UserDefaultKeys.IsFirstInstall) as? Bool ?? true
+    }
+    
+    func appLaunched() {
+        UserDefaults.standard.set(false, forKey: UserDefaultKeys.IsFirstInstall)
+    }
+}
---
+++ Conjugate/UserDefaultKeys.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaultKeys.swift
+//  Conjugate
+//
+//  Created by Halil Gursoy on 31/10/2016.
+//  Copyright © 2016 Adel  Shehadeh. All rights reserved.
+//
+
+import Foundation
+
+
+struct UserDefaultKeys {
+    static let IsFirstInstall = "FirstInstall"
+}
