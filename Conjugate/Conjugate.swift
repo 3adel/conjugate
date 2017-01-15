df3e4752ae75145b201fe030e235453dffdfdedf
Halil <halil.gursoy@zalando-lounge.de>
2017-01-15 23:02:17 +0100

    Added methods for searching in the interface language for presenter and view controllers

--- Conjugate/Conjugate.swift
+++ Conjugate/Conjugate.swift
@@ -23,12 +23,16 @@ class ConjugateViewController: UIViewController {
     var viewModel = ConjugateViewModel.empty
     var searchText = ""
     
+    var translationTableView: UITableView?
+    var translationOverlayView: UIView?
+    var translationDataSource: TranslationDataSource?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         setupPresenter()
         
-        updateUI(with: viewModel)
+        presenter.getInitialData()
         
         let launchChecker = AppLaunchChecker()
         if launchChecker.isFirstInstall {
@@ -49,6 +53,8 @@ class ConjugateViewController: UIViewController {
     override func setupUI() {
         super.setupUI()
         
+        setupSearchLanguageSwitch()
+        
         searchField.locale = "de_DE"
         
         navigationController?.isNavigationBarHidden = true
@@ -64,12 +70,10 @@ class ConjugateViewController: UIViewController {
         searchField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
         
         alertHandler = AlertHandler(view: view, topLayoutGuide: topLayoutGuide, bottomLayoutGuide: bottomLayoutGuide)
-        
-        searchLanguageSwitch.offLabel.text = "DE"
-        searchLanguageSwitch.onLabel.text = "EN"
-        
-        searchLanguageSwitch.onThumbImage = UIImage(named: "en_flag")
-        searchLanguageSwitch.offThumbImage = UIImage(named: "de_flag")
+    }
+    
+    func setupSearchLanguageSwitch() {
+        searchLanguageSwitch.addTarget(self, action: #selector(switchChanged(_:)), for: .valueChanged)
     }
     
     func search() {
@@ -92,11 +96,54 @@ class ConjugateViewController: UIViewController {
         presenter = ConjugatePresenter(view: self)
         verbDetailViewController?.presenter = presenter
     }
+    
+    func switchChanged(_ sender: SevenSwitch) {
+        let language = sender.isOn() ? viewModel.switchInterfaceLanguage : viewModel.switchSearchLanguage
+        presenter.searchLanguageChanged(to: language)
+    }
 }
 
 extension ConjugateViewController: ConjugateView {
+    func update(searchFieldPlaceholder: String) {
+        searchField.placeholder = searchFieldPlaceholder
+    }
+    
+    func render(with viewModel: TranslationsViewModel) {
+        if let translationTableView = translationTableView,
+            let dataSource = translationDataSource {
+            
+            dataSource.translations = viewModel.translations
+            translationTableView.reloadData()
+            
+            changeTranslationTableViewHeight(translationTableView, dataSource: dataSource)
+        } else {
+            setupTranslationTableView(with: searchView)
+            
+            guard let tableView = translationTableView else { return }
+            setupTranslationDataSource(for: tableView)
+            
+            translationDataSource?.translations = viewModel.translations
+            tableView.reloadData()
+            
+            guard let overlayView = translationOverlayView,
+                let dataSource = translationDataSource
+                else { return }
+            
+            showTranslationTableView(tableView, searchView: searchView, overlayView: overlayView, dataSource: dataSource)
+        }
+    }
     func updateUI(with viewModel: ConjugateViewModel) {
         self.viewModel = viewModel
+        
+        searchField.placeholder = viewModel.searchFieldPlaceholder
+        
+        
+        searchLanguageSwitch.offLabel.text = viewModel.switchSearchLanguage
+        searchLanguageSwitch.onLabel.text = viewModel.switchInterfaceLanguage
+        
+        searchLanguageSwitch.onThumbImage = UIImage(named: viewModel.switchInterfaceLanguageFlagImage)
+        searchLanguageSwitch.offThumbImage = UIImage(named: viewModel.switchLanguageFlagImage)
+        
         verbDetailViewController?.updateUI(with: viewModel)
     }
     
@@ -136,12 +183,114 @@ extension ConjugateViewController {
 }
 
 extension ConjugateViewController {
+    func setupTranslationTableView(with searchView: UIView) {
+        let defaultHeight: CGFloat = 200
+        
+        let frame = CGRect(origin: CGPoint.zero, size: CGSize(width: searchView.frame.width, height: defaultHeight))
+        translationTableView = UITableView(frame: frame, style: .plain)
+        translationTableView?.layer.cornerRadius = 6
+        translationTableView?.layer.borderColor = searchView.layer.borderColor
+        translationTableView?.layer.borderWidth = searchView.layer.borderWidth
+        
+        translationOverlayView = UIView(frame: view.bounds)
+        translationOverlayView?.backgroundColor = UIColor.darkGray
+        translationOverlayView?.alpha = 0.3
+        
+        let tapRec = UITapGestureRecognizer(target: self, action: #selector(hideTranslationTableView))
+        translationOverlayView?.addGestureRecognizer(tapRec)
+    }
+    
+    func setupTranslationDataSource(for tableView: UITableView) {
+        translationDataSource = TranslationDataSource(tableView: tableView)
+        translationDataSource?.onTranslationSelected = { [weak self] index in
+            self?.translationSelected(index: index)
+        }
+        
+        tableView.dataSource = translationDataSource
+        tableView.delegate = translationDataSource
+    }
+    
+    func showTranslationTableView(_ tableView: UITableView, searchView: UIView, overlayView: UIView, dataSource: TranslationDataSource) {
+        let yOrigin = searchView.frame.origin.y + searchView.frame.height - 5
+        let xOrigin = searchView.frame.origin.x
+        
+        let origin = CGPoint(x: xOrigin, y: yOrigin)
+        let size = CGSize(width: tableView.frame.width, height: 0)
+        
+        let initialFrame = CGRect(origin: origin, size: size)
+        
+        let height = heightForTranslationTableView(tableView, dataSource: dataSource)
+        let newFrame = CGRect(origin: origin, size: CGSize(width: size.width, height: height))
+        
+        tableView.frame = initialFrame
+        
+        overlayView.alpha = 0
+        
+        view.insertSubview(overlayView, belowSubview: searchView)
+        view.insertSubview(tableView, belowSubview: searchView)
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            overlayView.alpha = 0.3
+            tableView.frame = newFrame
+        })
+    }
+    
+    func heightForTranslationTableView(_ tableView: UITableView, dataSource: TranslationDataSource) -> CGFloat {
+        let expectedHeight = dataSource.contentHeight
+        let bottomPadding: CGFloat = 100
+        
+        //TODO: Get keyboard height dynamically
+        let keyboardHeight: CGFloat = 250
+        
+        let maxHeight = view.frame.height - keyboardHeight - tableView.frame.origin.y - bottomPadding
+        
+        let height = min(maxHeight, expectedHeight)
+        return height
+    }
+    
+    func changeTranslationTableViewHeight(_ tableView: UITableView, dataSource: TranslationDataSource) {
+        let height = heightForTranslationTableView(tableView, dataSource: dataSource)
+        
+        UIView.animate(withDuration: 0.3) {
+            tableView.frame.size.height = height
+        }
+    }
+    
+    func translationSelected(index: Int) {
+        hideTranslationTableView()
+        presenter.translationSelected(at: index)
+    }
+    
+    func hideTranslationTableView() {
+        guard let tableView = translationTableView,
+            let overlayView = translationOverlayView
+            else { return }
+        
+        var lastFrame = tableView.frame
+        lastFrame.size.height = 0
+        
+        let overlayAlpha: CGFloat = 0
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            tableView.frame = lastFrame
+            overlayView.alpha = overlayAlpha
+        }) { _ in
+            tableView.removeFromSuperview()
+            overlayView.removeFromSuperview()
+            
+            self.translationTableView = nil
+            self.translationOverlayView = nil
+        }
+    }
+}
+
+extension ConjugateViewController {
     func showActionsForForm(inTab tab: Int, atTense tense: Int,  at index: Int) {
         verbDetailViewController?.showActionsForForm(inTab: tab, atTense: tense, at: index)
     }
 }
 
-extension ConjugateViewController {
+extension ConjugateViewController {    
     func textFieldDidChange(_ textField: UITextField) {
         hideErrorMessage()
         
