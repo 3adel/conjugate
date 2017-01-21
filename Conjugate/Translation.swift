4cfd4314b7d741fabd2fe5ca535bffaf46b13a8b
Halil Gursoy <halil.gursoy@zalando-lounge.de>
2017-01-21 12:25:43 +0100

    Added Translation model

---
+++ Conjugate/Translation.swift
@@ -0,0 +1,14 @@
+//
+//  Translation.swift
+//  Conjugate
+//
+//  Created by Halil Gursoy on 21/01/2017.
+//  Copyright © 2017 Adel  Shehadeh. All rights reserved.
+//
+
+import Foundation
+
+struct Translation {
+    let verb: String
+    let meaning: String
+}
